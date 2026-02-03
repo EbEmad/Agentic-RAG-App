c92f8cc6e892ca05aef7baf3014fda35c71d3fc1
Ebrahim Emad <ebemad8@gmail.com>
2026-02-03 10:24:05 +0200

    edit

--- Terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "s3_bucket_name" {
-  value = aws_s3_bucket.mlops_bucket.id
-}
-
-output "ec2_public_ip" {
-  value = aws_instance.mlops_vm.public_ip
-}
--- Terraform/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "mlops_bucket" {
-  bucket = var.s3_bucket_name
-
-  tags = {
-    Name        = "mlops-artifact-store"
-    Environment = "dev"
-  }
-}
